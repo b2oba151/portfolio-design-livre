bd6aecd87a1e87377eef860487e803822515c850
b2oba151 <bodiarra20@gmail.com>
2024-02-27 06:27:40 +0100

    Javascript Transition des pages ajouté

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
 import '/public/style.css'
 import viteLogo from '/public/vite.svg'
 
+const pageTurnBtn = document.querySelectorAll('.nextprev-btn')
+
+pageTurnBtn.forEach((el, index) => {
+    el.onclick = () => {
+        const pageTurnId = el.getAttribute('data-page')
+        const pageTurn = document.getElementById(pageTurnId)
+
+        if (pageTurn.classList.contains('turn')) {
+            pageTurn.classList.remove('turn')
+            setTimeout(() => {
+                pageTurn.style.zIndex = 20 - index;
+            },500)
+        } else {
+            pageTurn.classList.add('turn')
+            setTimeout(() => {
+                pageTurn.style.zIndex = 20 + index;
+            },500)
+        }
+    }
+})
