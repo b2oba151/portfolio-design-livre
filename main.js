e2915b83b76ead6e65fb1d3f47ca1912f4651f74
b2oba151 <bodiarra20@gmail.com>
2024-02-27 06:55:07 +0100

    Update main.js
    De contactez moi vers profile

--- main.js
+++ main.js
@@ -41,6 +41,39 @@ contactMeBtn.onclick = () => {
 }
 
 
+// reverse index function
+let totalPages = pages.length
+let pageNumber = 0
+
+function reverseIndex() {
+  pageNumber--
+  if (pageNumber < 0 ) {
+    pageNumber = totalPages -1
+  }
+
+}
+
+
+// profile button
+
+const backProfileBtn = document.querySelector('.back-profile')
+backProfileBtn.onclick = () => {
+    pages.forEach((_, index) => {
+        setTimeout(() => {
+            reverseIndex()
+            pages[pageNumber].classList.remove('turn')
+
+            setTimeout(() => {
+                reverseIndex()
+                pages[pageNumber].style.zIndex = 10 + index
+            },500)
+        }, (index+1)*200+100);
+    })
+}
+
+
+
+
 
 
 
